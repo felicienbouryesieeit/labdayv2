0a958f6d7a687725ada36c7b9ebefbcf1cbfa96e
JinSeeng <sellia.nade@gmail.com>
2025-01-30 09:06:40 +0100

    Fix bug

--- labdaygame/option_menu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_button_2_pressed() -> void:
-	print("en cours de céation")
-
-func _on_button_4_pressed() -> void:
-	get_tree().change_scene_to_file("res://main_menu.tscn")
-	
