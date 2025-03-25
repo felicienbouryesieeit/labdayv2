099265a9b7c01603aae9fcf8fc20dc1dca73ec6e
Rom1 <romain.descha2006@gmail.com>
2025-03-25 11:09:33 +0100

    add sound in menu and menu option

--- labdaygame/option_menu.gd
+++ labdaygame/option_menu.gd
@@ -1,7 +1,19 @@
 extends Control
-	
-func _on_button_2_pressed() -> void:
-	print("en cours de céation")
 
-func _on_button_4_pressed() -> void:
+func _on_music_button_pressed() -> void:
+	print("en cours de céation")
+	
+func _on_music_button_mouse_entered() -> void:
+	$hover.play()
+
+func _on_option_button_pressed() -> void:
+	print("en cours de céation")
+	
+func _on_option_button_mouse_entered() -> void:
+	$hover.play()
+
+func _on_return_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+func _on_return_button_mouse_entered() -> void:
+	$hover.play()
