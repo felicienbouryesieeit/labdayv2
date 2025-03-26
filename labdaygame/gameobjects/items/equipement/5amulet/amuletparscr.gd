94f80261fc04d29219574326510d4d9c6e29d90f
Rom1 <romain.descha2006@gmail.com>
2025-03-26 19:52:32 +0100

    add items

--- labdaygame/gameobjects/items/equipement/5amulet/amuletparscr.gd
+++ labdaygame/gameobjects/items/equipement/5amulet/amuletparscr.gd
@@ -4,3 +4,6 @@ func _ready() -> void:
 	super._ready()
 	setequipmenttype(5)
 	
+func beginitem():
+	super.beginitem()
+	imagepath = "res://Ninja Adventure - Asset Pack/Ui/Icon/Items&Weapon/Amulet"
---
+++ labdaygame/gameobjects/items/equipement/5amulet/ironamulet.gd
@@ -0,0 +1,8 @@
+extends "res://gameobjects/items/equipement/5amulet/amuletparscr.gd"
+
+func _ready() -> void:
+	super._ready()
+	nameindex=18
+	descriptionindex=19
+	setstats(0,0,1,0,0,0)
+	
